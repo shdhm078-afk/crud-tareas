a61f0db8c5c3ae7138ba36194e147a01f8267017
shdhm078-afk <shdhm078@gmail.com>
2026-06-07 14:28:36 +0200

    Commit incorrecto con error grave

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+// ERROR: esta línea no debería estar aquí
+$variableError = "esto rompe el proyecto";
 $file = 'tareas.json';
 $tareas = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
 
